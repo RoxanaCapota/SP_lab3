d6da6a6846b90967ef984a1d2d3ce4a21444bbad
RoxanaCapota <73850383+RoxanaCapota@users.noreply.github.com>
2022-01-12 11:42:25 +0200

    Add project files.

---
+++ DessignPatternsHomework/Models/Image.cs
@@ -0,0 +1,48 @@
+﻿using DessignPatternsHomework.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DessignPatternsHomework.Models
+{
+    public class Image : IElement
+    {
+        #region Properties
+        public string ImageName { get; set; }
+        #endregion
+
+        #region Constructors
+        public Image()
+        {
+
+        }
+
+        public Image(string imageName)
+        {
+            ImageName = imageName;
+        }
+        #endregion
+
+        #region Methods
+        public void Print() 
+        {
+            Console.WriteLine($"Image with name: {ImageName}");
+        }
+
+        public void Add(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(IElement element)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IElement Get(int index)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ DessignPatternsHomework/Program.cs
@@ -0,0 +1,40 @@
+﻿using DessignPatternsHomework.Interfaces;
+using DessignPatternsHomework.Models;
+using System;
+using System.Linq;
+
+namespace DessignPatternsHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book noapteBuna = new Book("Noapte buna, copii!");
+            Author rpGheo = new Author("Radu Pavel Gheo");
+
+            noapteBuna.AddAuthor(rpGheo);
+
+            Section cap1 = new Section("Capitolul 1");
+            Section cap11 = new Section("Capitolul 1.1");
+            Section cap111 = new Section("Capitolul 1.1.1");
+            Section cap1111 = new Section("Capitolul 1.1.1.1");
+
+            noapteBuna.AddContent(new Paragraph("Multumesc celor care..."));
+            noapteBuna.AddContent(cap1);
+
+            cap1.Add(new Paragraph("Moto capitol"));
+            cap1.Add(cap11);
+
+            cap11.Add(new Paragraph("Text from subchapter 1.1"));
+            cap11.Add(cap111);
+
+            cap111.Add(new Paragraph("Text from subchapter 1.1.1"));
+            cap111.Add(cap1111);
+
+            cap1111.Add(new Image("Image subchapter 1.1.1.1"));
+
+            noapteBuna.Print();
+        }
+    }
+}
